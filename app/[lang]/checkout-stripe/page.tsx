804946290bf1522564a172ef267f8435e09e639b
Aguti1902 <aguti1902@users.noreply.github.com>
2026-01-14 12:50:19 +0100

    Remove Whop integration - Restore original Stripe checkout

--- app/[lang]/checkout-stripe/page.tsx
+++ app/[lang]/checkout-stripe/page.tsx
@@ -1,8 +1,7 @@
 // app/[lang]/checkout-stripe/page.tsx
-// MIGRADO A WHOP - Este archivo redirige al nuevo checkout
-import CheckoutWhop from '../checkout/checkout-whop'
+import CheckoutStripe from '../checkout/checkout-stripe'
 
 export default function CheckoutStripePage() {
-  return <CheckoutWhop />
+  return <CheckoutStripe />
 }
 
